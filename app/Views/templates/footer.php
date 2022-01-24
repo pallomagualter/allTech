2ee810473be7b74dc24736e2f1b0bc98948b543f
pallomagualter <pallomagualter@gmail.com>
2022-01-24 12:12:38 -0300

    utilizando template

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -23,10 +23,10 @@
 <!-- REQUIRED SCRIPTS -->
 
 <!-- jQuery -->
-<script src="plugins/jquery/jquery.min.js"></script>
+<script src="<?= base_url('theme/plugins/jquery/jquery.min.js') ?>"></script>
 <!-- Bootstrap 4 -->
-<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?= base_url('theme/plugins/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
 <!-- AdminLTE App -->
-<script src="dist/js/adminlte.min.js"></script>
+<script src="<?= base_url('theme/dist/js/adminlte.min.js') ?>"></script>
 </body>
 </html>
