6f97e0bed39612b16bc395552547b091bc064528
pallomagualter <pallomagualter@gmail.com>
2022-01-24 11:42:15 -0300

    incluindo navbar e sidebar

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -18,3 +18,6 @@ scratch. This page gets rid of all links and provides the needed markup only.
 </head>
 <body class="hold-transition sidebar-mini">
 <div class="wrapper">
+
+<?php include_once('navbar.php') ?>
+<?php include_once('sidebar.php') ?>
